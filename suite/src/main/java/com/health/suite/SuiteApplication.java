474980f0976cf93f5d87032629723b01a60c7a74
rkrumova97 <rkrumova97@gmail.com>
2021-12-04 14:25:45 +0100

    Spring project initialization

---
+++ suite/src/main/java/com/health/suite/SuiteApplication.java
@@ -0,0 +1,13 @@
+package com.health.suite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiteApplication.class, args);
+	}
+
+}
---
+++ suite/src/test/java/com/health/suite/SuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
